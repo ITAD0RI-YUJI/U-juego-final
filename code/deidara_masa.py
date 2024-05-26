c9271cf5369e6ec459104dbdd54c737ffc0c0e99
Sebaaa._.as <100049059+ITAD0RI-YUJI@users.noreply.github.com>
2024-05-26 15:43:26 -0500

    creando clase abstracta para los pets

--- code/deidara_masa.py
+++ code/deidara_masa.py
@@ -3,6 +3,7 @@ import random
 from pantalla import ancho_pantalla , alto_pantalla
 from config import *
 from pets_sprites import *
+from abc import ABC, abstractmethod
 
 from pygame.sprite import Sprite
 
@@ -11,25 +12,48 @@ pygame.init()
 #Arreglos para controlar la cant de pets
 bats_array = []
 
-#Clases de los pets
-class Bat(Sprite):
-    def __init__(self , bat_animation , animation_time , bats):
-        self.vida = 1
-
-        self.images_animation = bat_animation
+#CLase abstracta para los pets
+class Pet(ABC , Sprite):
+    def __init__(self , animation , animation_time , pet):
+        
+        self.images_animation = animation
         self.current_image = 0
         self.personaje_imagen = self.images_animation[self.current_image]
         self.rect = self.personaje_imagen.get_rect()
-        self.rect.y = random.randrange(alto_pantalla - self.rect.height)
-        self.rect.x = 680
-        
-        self.audio = pygame.mixer.Sound("../multimedia/audio/bat_sound.mp3")
-
+    
         self.animation_time = animation_time  # Tiempo en milisegundos para cambiar la imagen
         self.last_update = pygame.time.get_ticks()
         
         self.contador = 0
-        self.bats_array = bats
+        self.bats_array = pet
+
+    @abstractmethod
+    def animacion(self):
+        pass
+
+    @abstractmethod
+    def draw(self):
+        pass
+
+    @abstractmethod
+    def mover(self):
+       pass
+
+    @abstractmethod
+    def delete(self):
+        pass
+
+
+#Clases de los pets
+class Bat(Pet):
+    def __init__(self , animation , animation_time , pet):
+        super().__init__(animation, animation_time, pet)
+        self.vida = 1
+
+        self.rect.y = random.randrange(alto_pantalla - self.rect.height)
+        self.rect.x = 680
+        
+        self.audio = pygame.mixer.Sound("../multimedia/audio/bat_sound.mp3")
 
         self.velocidad_x = 3
 
